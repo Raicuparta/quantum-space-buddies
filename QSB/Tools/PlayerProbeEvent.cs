8032e7b7e60b90c4280d9b76379ccd5edb25ea8f
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-08-14 17:44:42 +0100

    aaa

--- QSB/Tools/PlayerProbeEvent.cs
+++ QSB/Tools/PlayerProbeEvent.cs
@@ -3,32 +3,61 @@ using QSB.Messaging;
 
 namespace QSB.Tools
 {
-    public class PlayerProbeEvent : QSBEvent<ToggleMessage>
+    public class PlayerProbeEvent : QSBEvent<ProbeMessage>
     {
         public override MessageType Type => MessageType.ProbeActiveChange;
 
         public override void SetupListener()
         {
-            GlobalMessenger<SurveyorProbe>.AddListener(EventNames.LaunchProbe, probe => SendEvent(CreateMessage(true)));
-            GlobalMessenger<SurveyorProbe>.AddListener(EventNames.RetrieveProbe, probe => SendEvent(CreateMessage(false)));
+            GlobalMessenger<SurveyorProbe>.AddListener(EventNames.LaunchProbe, probe => SendEvent(CreateMessage(ProbeData.Launch)));
+            GlobalMessenger<SurveyorProbe>.AddListener(EventNames.RetrieveProbe, probe => SendEvent(CreateMessage(ProbeData.Retrieve)));
         }
 
-        private ToggleMessage CreateMessage(bool value) => new ToggleMessage
+        private ProbeMessage CreateMessage(ProbeData value) => new ProbeMessage
         {
             SenderId = LocalPlayerId,
-            ToggleValue = value
+            Value = value
         };
 
-        public override void OnReceiveRemote(ToggleMessage message)
+        public override void OnReceiveRemote(ProbeMessage message)
         {
             var player = PlayerRegistry.GetPlayer(message.SenderId);
-            player.UpdateState(State.ProbeActive, message.ToggleValue);
-            player.Probe.SetState(message.ToggleValue);
+            switch (message.Value)
+            {
+                case ProbeData.Launch:
+                    player.UpdateState(State.ProbeActive, true);
+                    player.Probe.SetState(true);
+                    break;
+                case ProbeData.Retrieve:
+                    player.UpdateState(State.ProbeActive, false);
+                    player.Probe.SetState(false);
+                    break;
+                case ProbeData.Anchor:
+                    break;
+                case ProbeData.Warp:
+                    break;
+            }
         }
 
-        public override void OnReceiveLocal(ToggleMessage message)
+        public override void OnReceiveLocal(ProbeMessage message)
         {
-            PlayerRegistry.LocalPlayer.UpdateState(State.ProbeActive, message.ToggleValue);
+            switch (message.Value)
+            {
+                case ProbeData.Launch:
+                    PlayerRegistry.LocalPlayer.UpdateState(State.ProbeActive, true);
+                    break;
+                case ProbeData.Retrieve:
+                    PlayerRegistry.LocalPlayer.UpdateState(State.ProbeActive, false);
+                    break;
+            }
         }
+    }
+
+    public enum ProbeData
+    {
+        Launch,
+        Retrieve,
+        Anchor,
+        Warp
     }
 }
---
+++ QSB/Tools/ProbeMessage.cs
@@ -0,0 +1,22 @@
+﻿using QSB.Tools;
+using UnityEngine.Networking;
+
+namespace QSB.Tools
+{
+    public class ProbeMessage : PlayerMessage
+    {
+        public ProbeData Value { get; set; }
+
+        public override void Deserialize(NetworkReader reader)
+        {
+            base.Deserialize(reader);
+            Value = reader.ReadInt32();
+        }
+
+        public override void Serialize(NetworkWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write(Value);
+        }
+    }
+}
